2538f22ecc55324dbec85de9527d03eab2d2dcf2
Gonca <124376316+goncinthehood@users.noreply.github.com>
2023-05-29 20:46:33 +0300

    Added ObstacleSpawner
    Its movements added and spawns fixed

---
+++ Assets/Scripts/LeftMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMovement : MonoBehaviour
+{
+    public float speed;
+    BoxCollider2D box;
+    float groundWidht;
+    float obstacleWidth;
+    void Start()
+    {
+        if (gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWidht = box.size.x;
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }
+
+    }
+
+
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        if (transform.position.x <= -groundWidht)
+        {
+            transform.position = new Vector2(transform.position.x + 2 * groundWidht, transform.position.y);
+        }
+
+    }
+}
